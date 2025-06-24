99c2fa8732e7bed61429c9be312094124f5f0823
Stephen Planck <splanck9271@gmail.com>
2025-06-24 17:46:06 -0400

    Add util helpers and refactor

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <getopt.h>
+#include "util.h"
 #include "token.h"
 #include "parser.h"
 #include "semantic.h"
@@ -27,38 +27,6 @@ static void print_usage(const char *prog)
     printf("      --dump-ir        Print IR to stdout and exit\n");
 }
 
-static char *read_file(const char *path)
-{
-    FILE *f = fopen(path, "rb");
-    if (!f)
-        return NULL;
-    if (fseek(f, 0, SEEK_END) != 0) {
-        fclose(f);
-        return NULL;
-    }
-    long len = ftell(f);
-    if (len < 0) {
-        fclose(f);
-        return NULL;
-    }
-    if (fseek(f, 0, SEEK_SET) != 0) {
-        fclose(f);
-        return NULL;
-    }
-    char *buf = malloc((size_t)len + 1);
-    if (!buf) {
-        fclose(f);
-        return NULL;
-    }
-    if (fread(buf, 1, (size_t)len, f) != (size_t)len) {
-        fclose(f);
-        free(buf);
-        return NULL;
-    }
-    buf[len] = '\0';
-    fclose(f);
-    return buf;
-}
 
 int main(int argc, char **argv)
 {
@@ -143,9 +111,9 @@ int main(int argc, char **argv)
 
     const char *source = argv[optind];
 
-    char *src_text = read_file(source);
+    char *src_text = vc_read_file(source);
     if (!src_text) {
-        perror("read_file");
+        perror("vc_read_file");
         return 1;
     }
 
