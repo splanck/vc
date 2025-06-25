3f279a83f960ba77336fd0a22e6ee54cb8073c7c
Stephen Planck <splanck9271@gmail.com>
2025-06-25 15:03:45 -0400

    Add x86-64 fixture files and tests

---
+++ tests/fixtures/const_load_x86-64.s
@@ -0,0 +1,10 @@
+main:
+    pushq %rbp
+    movq %rsp, %rbp
+    movq $5, %rax
+    movq %rax, x
+    movq $5, %rax
+    movq %rax, y
+    movq $5, %rax
+    movq %rax, %rax
+    ret
---
+++ tests/fixtures/simple_add_x86-64.s
@@ -0,0 +1,6 @@
+main:
+    pushq %rbp
+    movq %rsp, %rbp
+    movq $7, %rax
+    movq %rax, %rax
+    ret
