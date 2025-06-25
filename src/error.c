f6ab2966dc545ae38bfab0d27fa179f7b2986d4b
Stephen Planck <splanck9271@gmail.com>
2025-06-24 21:08:43 -0400

    Document error reporting utilities

--- src/error.c
+++ src/error.c
@@ -8,15 +8,26 @@
 #include <stdio.h>
 #include "error.h"
 
+/* Stored source location for the next error message */
 static size_t error_line = 0;
 static size_t error_column = 0;
 
+/*
+ * Remember the given source position for use by error_print().  The
+ * values are 1-based; a line or column of 0 indicates an unknown
+ * position.
+ */
 void error_set(size_t line, size_t col)
 {
     error_line = line;
     error_column = col;
 }
 
+/*
+ * Output "msg" followed by the location previously stored with
+ * error_set().  The message is printed to stderr in a
+ * "line/column" format.
+ */
 void error_print(const char *msg)
 {
     fprintf(stderr, "%s at line %zu, column %zu\n", msg, error_line, error_column);
