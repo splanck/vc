e34175aa76b586f9f87643ee593be0180b7cdc72
Stephen Planck <splanck9271@gmail.com>
2025-06-26 11:53:39 -0400

    support string literals as char pointers

---
+++ tests/fixtures/string_usage.s
@@ -0,0 +1,13 @@
+.data
+Lstr1:
+    .asciz "hi"
+.text
+main:
+    pushl %ebp
+    movl %esp, %ebp
+    movl $Lstr1, %eax
+    movl %eax, p
+    movl p, %eax
+    movl (%eax), %ebx
+    movl %ebx, %eax
+    ret
