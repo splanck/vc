cb5bff836f0edcd78c852c72231d4800bf1875c9
Stephen Planck <splanck9271@gmail.com>
2025-06-26 11:45:43 -0400

    Add switch statement integration fixture

---
+++ tests/fixtures/switch_basic.c
@@ -0,0 +1,9 @@
+int main() {
+    int x = 2;
+    switch (x) {
+    case 1:
+        return 3;
+    default :
+        return 0;
+    }
+}
---
+++ tests/fixtures/switch_basic.s
@@ -0,0 +1,19 @@
+main:
+    pushl %ebp
+    movl %esp, %ebp
+    movl $2, %eax
+    movl %eax, x
+    movl $0, %eax
+    cmpl $0, %eax
+    je L0_case0
+    jmp L0_default
+L0_case0:
+    movl $3, %eax
+    movl %eax, %eax
+    ret
+    jmp L0_end
+L0_default:
+    movl $0, %eax
+    movl %eax, %eax
+    ret
+L0_end:
