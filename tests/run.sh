5649ea1d84ff2e032e31d6f0b00db90f0c8a4be4
Stephen Planck <splanck9271@gmail.com>
2025-06-27 00:22:44 -0400

    Update unit test build sources

--- tests/run.sh
+++ tests/run.sh
@@ -6,8 +6,11 @@ make
 # build unit test binary
 cc -Iinclude -Wall -Wextra -std=c99 \
     -o "$DIR/unit_tests" "$DIR/unit/test_lexer_parser.c" \
-    src/lexer.c src/parser.c src/parser_expr.c src/parser_stmt.c \
-    src/parser_types.c src/symtable.c src/ast.c src/util.c \
+    src/parser_core.c src/parser_init.c src/parser_decl.c \
+    src/parser_flow.c src/parser_toplevel.c src/parser_expr.c \
+    src/parser_stmt.c src/parser_types.c src/symtable_core.c \
+    src/symtable_globals.c src/symtable_struct.c src/ast_clone.c \
+    src/ast_expr.c src/ast_stmt.c src/lexer.c src/util.c \
     src/vector.c src/error.c
 # run unit tests
 "$DIR/unit_tests"
