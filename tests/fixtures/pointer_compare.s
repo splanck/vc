16176fb32b4499fe18797b07810ef49e18242180
Stephen Planck <splanck9271@gmail.com>
2025-06-28 15:36:16 -0400

    Add pointer comparison and subtraction fixtures

---
+++ tests/fixtures/pointer_compare.s
@@ -0,0 +1,22 @@
+main:
+    pushl %ebp
+    movl %esp, %ebp
+    movl $arr, %eax
+    movl %eax, p1
+    movl $arr, %eax
+    movl $1, %ebx
+    movl %ebx, %ecx
+    imull $4, %ecx
+    addl %eax, %ecx
+    movl %ecx, p2
+    movl p1, %ecx
+    movl p2, %ebx
+    movl %ecx, %eax
+    cmpl %ebx, %eax
+    setl %al
+    movzbl %al, %eax
+    movl %eax, %eax
+    ret
+    movl %ebp, %esp
+    popl %ebp
+    ret
