d3ad201c34a76b12843b197c440be5cf83f67662
Stephen Planck <splanck9271@gmail.com>
2025-07-09 21:39:01 -0500

    refactor stmt dispatch

--- src/semantic_stmt.c
+++ src/semantic_stmt.c
@@ -510,6 +510,207 @@ static int check_var_decl_stmt_wrapper(stmt_t *stmt, symtable_t *vars,
     return check_var_decl_stmt(stmt, vars, funcs, ir);
 }
 
+typedef int (*stmt_handler_fn)(stmt_t *stmt, symtable_t *vars,
+                               symtable_t *funcs, label_table_t *labels,
+                               ir_builder_t *ir, type_kind_t func_ret_type,
+                               const char *break_label,
+                               const char *continue_label);
+
+/* Dispatch wrappers matching stmt_handler_fn */
+static int stmt_expr_handler(stmt_t *stmt, symtable_t *vars, symtable_t *funcs,
+                             label_table_t *labels, ir_builder_t *ir,
+                             type_kind_t func_ret_type, const char *break_label,
+                             const char *continue_label)
+{
+    (void)labels; (void)func_ret_type; (void)break_label; (void)continue_label;
+    return check_expr_stmt(stmt, vars, funcs, ir);
+}
+
+static int stmt_return_handler(stmt_t *stmt, symtable_t *vars, symtable_t *funcs,
+                               label_table_t *labels, ir_builder_t *ir,
+                               type_kind_t func_ret_type,
+                               const char *break_label,
+                               const char *continue_label)
+{
+    (void)labels; (void)break_label; (void)continue_label;
+    return check_return_stmt(stmt, vars, funcs, ir, func_ret_type);
+}
+
+static int stmt_var_decl_handler(stmt_t *stmt, symtable_t *vars,
+                                 symtable_t *funcs, label_table_t *labels,
+                                 ir_builder_t *ir, type_kind_t func_ret_type,
+                                 const char *break_label,
+                                 const char *continue_label)
+{
+    (void)labels; (void)func_ret_type; (void)break_label; (void)continue_label;
+    return check_var_decl_stmt_wrapper(stmt, vars, funcs, ir);
+}
+
+static int stmt_while_handler(stmt_t *stmt, symtable_t *vars, symtable_t *funcs,
+                              label_table_t *labels, ir_builder_t *ir,
+                              type_kind_t func_ret_type,
+                              const char *break_label,
+                              const char *continue_label)
+{
+    (void)break_label; (void)continue_label;
+    return check_while_stmt_wrapper(stmt, vars, funcs, labels, ir,
+                                    func_ret_type);
+}
+
+static int stmt_do_while_handler(stmt_t *stmt, symtable_t *vars,
+                                 symtable_t *funcs, label_table_t *labels,
+                                 ir_builder_t *ir, type_kind_t func_ret_type,
+                                 const char *break_label,
+                                 const char *continue_label)
+{
+    (void)break_label; (void)continue_label;
+    return check_do_while_stmt_wrapper(stmt, vars, funcs, labels, ir,
+                                       func_ret_type);
+}
+
+static int stmt_for_handler(stmt_t *stmt, symtable_t *vars, symtable_t *funcs,
+                            label_table_t *labels, ir_builder_t *ir,
+                            type_kind_t func_ret_type,
+                            const char *break_label,
+                            const char *continue_label)
+{
+    (void)break_label; (void)continue_label;
+    return check_for_stmt_wrapper(stmt, vars, funcs, labels, ir,
+                                  func_ret_type);
+}
+
+static int stmt_switch_handler(stmt_t *stmt, symtable_t *vars, symtable_t *funcs,
+                               label_table_t *labels, ir_builder_t *ir,
+                               type_kind_t func_ret_type,
+                               const char *break_label,
+                               const char *continue_label)
+{
+    (void)break_label; (void)continue_label;
+    return check_switch_stmt_wrapper(stmt, vars, funcs, labels, ir,
+                                     func_ret_type);
+}
+
+static int stmt_label_handler(stmt_t *stmt, symtable_t *vars, symtable_t *funcs,
+                              label_table_t *labels, ir_builder_t *ir,
+                              type_kind_t func_ret_type,
+                              const char *break_label,
+                              const char *continue_label)
+{
+    (void)vars; (void)funcs; (void)func_ret_type;
+    (void)break_label; (void)continue_label;
+    return handle_label_stmt(stmt, labels, ir);
+}
+
+static int stmt_goto_handler(stmt_t *stmt, symtable_t *vars, symtable_t *funcs,
+                             label_table_t *labels, ir_builder_t *ir,
+                             type_kind_t func_ret_type,
+                             const char *break_label,
+                             const char *continue_label)
+{
+    (void)vars; (void)funcs; (void)func_ret_type;
+    (void)break_label; (void)continue_label;
+    return check_goto_stmt(stmt, labels, ir);
+}
+
+static int stmt_static_assert_handler(stmt_t *stmt, symtable_t *vars,
+                                      symtable_t *funcs, label_table_t *labels,
+                                      ir_builder_t *ir,
+                                      type_kind_t func_ret_type,
+                                      const char *break_label,
+                                      const char *continue_label)
+{
+    (void)funcs; (void)labels; (void)ir; (void)func_ret_type;
+    (void)break_label; (void)continue_label;
+    return check_static_assert_stmt(stmt, vars);
+}
+
+static int stmt_typedef_handler(stmt_t *stmt, symtable_t *vars,
+                                symtable_t *funcs, label_table_t *labels,
+                                ir_builder_t *ir, type_kind_t func_ret_type,
+                                const char *break_label,
+                                const char *continue_label)
+{
+    (void)funcs; (void)labels; (void)ir; (void)func_ret_type;
+    (void)break_label; (void)continue_label;
+    return check_typedef_stmt_wrapper(stmt, vars);
+}
+
+static int stmt_enum_decl_handler(stmt_t *stmt, symtable_t *vars,
+                                  symtable_t *funcs, label_table_t *labels,
+                                  ir_builder_t *ir, type_kind_t func_ret_type,
+                                  const char *break_label,
+                                  const char *continue_label)
+{
+    (void)funcs; (void)labels; (void)ir; (void)func_ret_type;
+    (void)break_label; (void)continue_label;
+    return check_enum_decl_stmt_wrapper(stmt, vars);
+}
+
+static int stmt_struct_decl_handler(stmt_t *stmt, symtable_t *vars,
+                                    symtable_t *funcs, label_table_t *labels,
+                                    ir_builder_t *ir, type_kind_t func_ret_type,
+                                    const char *break_label,
+                                    const char *continue_label)
+{
+    (void)funcs; (void)labels; (void)ir; (void)func_ret_type;
+    (void)break_label; (void)continue_label;
+    return check_struct_decl_stmt_wrapper(stmt, vars);
+}
+
+static int stmt_union_decl_handler(stmt_t *stmt, symtable_t *vars,
+                                   symtable_t *funcs, label_table_t *labels,
+                                   ir_builder_t *ir, type_kind_t func_ret_type,
+                                   const char *break_label,
+                                   const char *continue_label)
+{
+    (void)funcs; (void)labels; (void)ir; (void)func_ret_type;
+    (void)break_label; (void)continue_label;
+    return check_union_decl_stmt_wrapper(stmt, vars);
+}
+
+static int stmt_break_handler(stmt_t *stmt, symtable_t *vars, symtable_t *funcs,
+                              label_table_t *labels, ir_builder_t *ir,
+                              type_kind_t func_ret_type,
+                              const char *break_label,
+                              const char *continue_label)
+{
+    (void)vars; (void)funcs; (void)labels; (void)ir; (void)func_ret_type;
+    (void)continue_label;
+    return check_break_stmt(stmt, break_label, ir);
+}
+
+static int stmt_continue_handler(stmt_t *stmt, symtable_t *vars,
+                                 symtable_t *funcs, label_table_t *labels,
+                                 ir_builder_t *ir, type_kind_t func_ret_type,
+                                 const char *break_label,
+                                 const char *continue_label)
+{
+    (void)vars; (void)funcs; (void)labels; (void)ir; (void)func_ret_type;
+    (void)break_label;
+    return check_continue_stmt(stmt, continue_label, ir);
+}
+
+static stmt_handler_fn stmt_handlers[] = {
+    [STMT_EXPR]        = stmt_expr_handler,
+    [STMT_RETURN]      = stmt_return_handler,
+    [STMT_VAR_DECL]    = stmt_var_decl_handler,
+    [STMT_IF]          = check_if_stmt_wrapper,
+    [STMT_WHILE]       = stmt_while_handler,
+    [STMT_DO_WHILE]    = stmt_do_while_handler,
+    [STMT_FOR]         = stmt_for_handler,
+    [STMT_SWITCH]      = stmt_switch_handler,
+    [STMT_BREAK]       = stmt_break_handler,
+    [STMT_CONTINUE]    = stmt_continue_handler,
+    [STMT_LABEL]       = stmt_label_handler,
+    [STMT_GOTO]        = stmt_goto_handler,
+    [STMT_STATIC_ASSERT] = stmt_static_assert_handler,
+    [STMT_TYPEDEF]     = stmt_typedef_handler,
+    [STMT_ENUM_DECL]   = stmt_enum_decl_handler,
+    [STMT_STRUCT_DECL] = stmt_struct_decl_handler,
+    [STMT_UNION_DECL]  = stmt_union_decl_handler,
+    [STMT_BLOCK]       = check_block_stmt_wrapper,
+};
+
 
 
 /*
@@ -526,51 +727,13 @@ int check_stmt(stmt_t *stmt, symtable_t *vars, symtable_t *funcs,
     if (!stmt)
         return 0;
     ir_builder_set_loc(ir, error_current_file, stmt->line, stmt->column);
-    switch (stmt->kind) {
-    case STMT_EXPR:
-        return check_expr_stmt(stmt, vars, funcs, ir);
-    case STMT_RETURN:
-        return check_return_stmt(stmt, vars, funcs, ir, func_ret_type);
-    case STMT_IF:
-        return check_if_stmt_wrapper(stmt, vars, funcs, labels, ir, func_ret_type,
-                                     break_label, continue_label);
-    case STMT_WHILE:
-        return check_while_stmt_wrapper(stmt, vars, funcs, labels, ir,
-                                        func_ret_type);
-    case STMT_DO_WHILE:
-        return check_do_while_stmt_wrapper(stmt, vars, funcs, labels, ir,
-                                           func_ret_type);
-    case STMT_FOR:
-        return check_for_stmt_wrapper(stmt, vars, funcs, labels, ir, func_ret_type);
-    case STMT_SWITCH:
-        return check_switch_stmt_wrapper(stmt, vars, funcs, labels, ir,
-                                         func_ret_type);
-    case STMT_LABEL:
-        return handle_label_stmt(stmt, labels, ir);
-    case STMT_GOTO:
-        return check_goto_stmt(stmt, labels, ir);
-    case STMT_STATIC_ASSERT:
-        return check_static_assert_stmt(stmt, vars);
-    case STMT_BREAK:
-        return check_break_stmt(stmt, break_label, ir);
-    case STMT_CONTINUE:
-        return check_continue_stmt(stmt, continue_label, ir);
-    case STMT_BLOCK:
-        return check_block_stmt_wrapper(stmt, vars, funcs, labels, ir,
-                                        func_ret_type, break_label,
-                                        continue_label);
-    case STMT_ENUM_DECL:
-        return check_enum_decl_stmt_wrapper(stmt, vars);
-    case STMT_STRUCT_DECL:
-        return check_struct_decl_stmt_wrapper(stmt, vars);
-    case STMT_UNION_DECL:
-        return check_union_decl_stmt_wrapper(stmt, vars);
-    case STMT_TYPEDEF:
-        return check_typedef_stmt_wrapper(stmt, vars);
-    case STMT_VAR_DECL:
-        return check_var_decl_stmt_wrapper(stmt, vars, funcs, ir);
-    }
-    return 0;
+    if ((size_t)stmt->kind >= sizeof(stmt_handlers) / sizeof(stmt_handlers[0]))
+        return 0;
+    stmt_handler_fn fn = stmt_handlers[stmt->kind];
+    if (!fn)
+        return 0;
+    return fn(stmt, vars, funcs, labels, ir, func_ret_type,
+              break_label, continue_label);
 }
 
 /* Issue warnings for unreachable statements after return or goto end */
