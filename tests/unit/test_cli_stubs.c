761f640a4061cd194a6c0e9f0b40d1c96fe19ee0
Stephen Planck <splanck9271@gmail.com>
2025-07-15 23:16:38 -0400

    tests: add stubs for cli unit tests

---
+++ tests/unit/test_cli_stubs.c
@@ -0,0 +1,6 @@
+#include "ast_stmt.h"
+#include "preproc_path.h"
+
+void ast_free_func(func_t *func) { (void)func; }
+void ast_free_stmt(stmt_t *stmt) { (void)stmt; }
+void preproc_set_internal_libc_dir(const char *path) { (void)path; }
