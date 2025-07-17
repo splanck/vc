b4e8de87ff3cce8ecf46f68981813186cee9dfdf
Stephen Planck <splanck9271@gmail.com>
2025-07-16 23:25:38 -0400

    Refactor VCFLAGS processing helpers

--- include/cli_env.h
+++ include/cli_env.h
@@ -2,6 +2,9 @@
 #define VC_CLI_ENV_H
 
 int load_vcflags(int *argc, char ***argv, char ***out_argv, char **out_buf);
+int count_vcflags_args(const char *env, size_t *out);
+char **build_vcflags_argv(char *vcbuf, int argc, char **argv,
+                          size_t vcargc);
 void scan_shortcuts(int *argc, char **argv);
 
 #endif /* VC_CLI_ENV_H */
