142c1633b78ceccdf824f8eaadbf5cb3d062369c
Stephen Planck <splanck9271@gmail.com>
2025-08-25 14:45:30 -0400

    libc: add perror implementation

---
+++ libc/src/perror.c
@@ -0,0 +1,47 @@
+#include <stddef.h>
+#include <string.h>
+#include "errno.h"
+#include "stdio.h"
+#include "../internal/_vc_syscalls.h"
+
+void perror(const char *msg)
+{
+    int err = *_errno_location();
+    char buf[64];
+    size_t pos = 0;
+
+    if (msg && *msg) {
+        size_t len = strlen(msg);
+        if (len > sizeof(buf) - 2)
+            len = sizeof(buf) - 2;
+        memcpy(buf, msg, len);
+        pos = len;
+        if (pos < sizeof(buf))
+            buf[pos++] = ':';
+        if (pos < sizeof(buf))
+            buf[pos++] = ' ';
+    }
+
+    char num[32];
+    char *p = num + sizeof(num);
+    unsigned int u = (unsigned int)err;
+    if (u == 0) {
+        *--p = '0';
+    } else {
+        while (u) {
+            *--p = (char)('0' + (u % 10));
+            u /= 10;
+        }
+    }
+    size_t nlen = (size_t)(num + sizeof(num) - p);
+    if (nlen > sizeof(buf) - pos - 1)
+        nlen = sizeof(buf) - pos - 1;
+    memcpy(buf + pos, p, nlen);
+    pos += nlen;
+
+    if (pos < sizeof(buf))
+        buf[pos++] = '\n';
+
+    _vc_write(2, buf, pos);
+}
+
