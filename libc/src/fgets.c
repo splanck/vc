9e692ba2b3664671a9208b507e69256b449b30c9
Stephen Planck <splanck9271@gmail.com>
2025-08-25 14:50:01 -0400

    Add fgets and tmpfile implementations

---
+++ libc/src/fgets.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+#include "../internal/_vc_syscalls.h"
+
+char *fgets(char *s, int size, FILE *stream)
+{
+    if (size <= 0)
+        return NULL;
+    int i = 0;
+    while (i < size - 1) {
+        char c;
+        long r = _vc_read(stream->fd, &c, 1);
+        if (r < 0) {
+            stream->err = 1;
+            return NULL;
+        }
+        if (r == 0) {
+            if (i == 0)
+                return NULL;
+            break;
+        }
+        s[i++] = c;
+        if (c == '\n')
+            break;
+    }
+    s[i] = '\0';
+    return s;
+}
+
