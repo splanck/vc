adca7cf0c811c898ed5cf1904506b0b878228dc3
Stephen Planck <splanck9271@gmail.com>
2025-08-27 22:55:05 -0400

    Add unit test for fgets EINTR

--- libc/src/fgets.c
+++ libc/src/fgets.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "errno.h"
 #include "../internal/_vc_syscalls.h"
 
 char *fgets(char *s, int size, FILE *stream)
@@ -8,13 +9,18 @@ char *fgets(char *s, int size, FILE *stream)
     int i = 0;
     while (i < size - 1) {
         char c;
-        long r = _vc_read(stream->fd, &c, 1);
+        long r;
+        do {
+            r = _vc_read(stream->fd, &c, 1);
+        } while (r < 0 && errno == EINTR);
         if (r < 0) {
             stream->err = 1;
+            stream->eof = 0;
             return NULL;
         }
         if (r == 0) {
             stream->eof = 1;
+            stream->err = 0;
             if (i == 0)
                 return NULL;
             break;
